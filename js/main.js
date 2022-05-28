d748340f1669ffe49dca3617d85599d9bad2281e
lebinhthuan <104605059+lebinhthuan@users.noreply.github.com>
2022-05-28 23:59:08 +0700

    them hieu ung giu banner khi cuon chuot

--- js/main.js
+++ js/main.js
@@ -39,18 +39,23 @@
     document.addEventListener("DOMContentLoaded",function() {
         let navbar = document.querySelector('.menu');
         let mount_position = "lessthan100px";
-        let product_info = document.querySelector('.product_info')
-        let banner_position = product_info.offsetTop;
-        let position_info = "bottom";
+
+        let product_adv = document.querySelector('.product_adv')
+        let banner_position = product_adv.offsetTop;
+        let position_info = "below";
+
+        let displayRange = 300;         // khoảng cách hiển thị đối với banner được chọn
+        let lowerLimit = banner_position + displayRange;
 
         window.addEventListener('scroll',function(){   //tac dong voi thanh menu
-            if (this.window.pageYOffset > 100) {
+            if  (this.window.pageYOffset > 100) {
                 if (mount_position == "lessthan100px") {
                     mount_position = "greaterthan100px";
                     navbar.classList.add('navbar-change');
                 }
 
-            } else if (this.window.pageYOffset < 100 ) {
+            } 
+            else if (this.window.pageYOffset < 100 ) {
                 if (mount_position == "greaterthan100px") {
                     mount_position = "lessthan100px";
                     navbar.classList.remove('navbar-change');
@@ -58,17 +63,18 @@
             }
         })
 
-        window.addEventListener('scroll',function(){ 
-            if (this.window.pageYOffset > 100) {
-                if (mount_position == "lessthan100px") {
-                    mount_position = "greaterthan100px";
-                    navbar.classList.add('navbar-change');
+        window.addEventListener('scroll',function(){    //tac dong voi banner ben cot noi dung ben phai
+            if  ((this.window.pageYOffset > banner_position) && (this.window.pageYOffset < lowerLimit)) {
+                if (position_info == "below") {
+                    position_info = "showing";
+                    product_adv.classList.add('stop_banner');
                 }
 
-            } else if (this.window.pageYOffset < 100 ) {
-                if (mount_position == "greaterthan100px") {
-                    mount_position = "lessthan100px";
-                    navbar.classList.remove('navbar-change');
+            } 
+            else if ((this.window.pageYOffset < banner_position ) || (this.window.pageYOffset > lowerLimit)){
+                if (position_info == "showing") {
+                    position_info = "below";
+                    product_adv.classList.remove('stop_banner');
                 }
             }
         })
